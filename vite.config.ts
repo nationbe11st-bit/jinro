8bc9efccea86af9c8bf50e6ff89f4e58bde00663
nationbe11st-bit <nationbe11st@gmail.com>
2026-03-08 21:42:07 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/jinro/', // 저장소 이름을 입력하세요. 앞뒤에 /가 반드시 있어야 합니다.
+    base: '/jinro/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
